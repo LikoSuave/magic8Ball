4186103cc684c9fe11d15a5b1cb3cdb2369cf30b
LikoSuave <mjmartin1997@gmail.com>
2020-12-15 15:13:22 -0500

    Added get answer function

--- magic8Ball.py
+++ magic8Ball.py
@@ -6,4 +6,26 @@
 
 import random
 
-def getAnswer(answerNumber)
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return 'It is certain'
+    elif answerNumber == 2:
+        return 'It is decidedly so'
+    elif answerNumber == 3:
+        return 'Yes'
+    elif answerNumber == 4:
+        return 'Reply hazy, try again'
+    elif answerNumber == 5:
+        return 'ASk again later'
+    elif answerNumber == 6:
+        return 'Concentrate and ask again'
+    elif answerNumber == 7:
+        return 'My reply is no'
+    elif answerNumber == 8:
+        return 'Outlook not so good'
+    elif answerNumber == 9:
+        return 'Very doubtful'
+
+print(getAnswer(random.randint(1,9)))
+
+# A function call can be used in an expression because the call evaluates to its return value
