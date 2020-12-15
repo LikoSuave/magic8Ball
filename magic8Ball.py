af44710f8eb33b9d8646dcbb5258ebc967d257ad
LikoSuave <mjmartin1997@gmail.com>
2020-12-14 19:40:57 -0500

    Import random

--- magic8Ball.py
+++ magic8Ball.py
@@ -1,1 +1,9 @@
 # Functions are mini programs inside programs
+# A value being passed to a function call is an arguement
+# Variables that have arguements assigned to them are parameters
+# The value that a function call evaluates to is called the return value
+# When using the def statment, you can specify  what the return value should be with a return statment
+
+import random
+
+def getAnswer(answerNumber)
